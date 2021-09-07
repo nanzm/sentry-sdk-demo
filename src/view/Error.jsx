f78c5d4cf3fdf4e3f02c00f1b51a208c5a87a2aa
nan <msg@nancode.cn>
2021-09-07 17:27:46 +0800

    update

--- src/view/Error.jsx
+++ src/view/Error.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { randomString } from "../lib/utils"
 
 function Error() {
   const handleError = () => {
-    const obj = JSON.parse("");
-    console.log(obj.name.b.c);
+    const obj = {};
+    console.log(obj.user.job);
   };
+
+  const handleError1 = () => {
+    let user = {}
+    user.detail[randomString(10)]
+  };
+
   return (
     <div className="section">
       <h1>Error</h1>
       <button onClick={() => handleError()}>undefined 按钮</button>
+      <button onClick={() => handleError1()}>随机错误 按钮</button>
     </div>
   );
 }
