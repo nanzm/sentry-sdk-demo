c0003f3563fbefbe9c74ea4a9eca74d3daf8856c
nan <msg@nancode.cn>
2022-01-04 21:39:23 +0800

    update

--- src/view/Error.jsx
+++ src/view/Error.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { randomString } from "../lib/utils"
 
+function Boo() {
+  throw new Error(title);
+}
+
 function Error() {
+  const [ isError, setError ] = React.useState(false);
+
   const handleError = () => {
     const obj = {};
     console.log(obj.user.job);
@@ -17,7 +23,11 @@ function Error() {
       <h1>Error</h1>
       <button onClick={() => handleError()}>undefined 按钮</button>
       <button onClick={() => handleError1()}>随机错误 按钮</button>
-      <button onClick={() => dd()}>崩溃 按钮</button>
+      <button onClick={() => {
+        setError(true)
+      }}>崩溃 按钮
+      </button>
+      {isError ? <Boo/> : null}
     </div>
   );
 }
